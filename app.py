32b4eb70c2bc97b4c7127a29276c933b5b2caf6d
hsalian <hpsalian13@gmail.com>
2022-09-30 16:46:26 +0530

    first commit

--- app.py
+++ app.py
@@ -34,10 +34,14 @@ def index():
 
             filename = searchString + ".csv"
             fw = open(filename, "w")
-            headers = "Product, Customer Name, Rating, Heading, Comment \n"
+            headers = "Price,Product, Customer Name, Rating, Heading, Comment \n"
             fw.write(headers)
             reviews = []
             for commentbox in commentboxes:
+                try:
+                    price = prodRes.find_all('div', {"class": "_30jeq3 _16Jk6d"})[0].text
+                except :
+                    price = 'no price available'
                 try:
                     #name.encode(encoding='utf-8')
                     name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
@@ -66,7 +70,7 @@ def index():
                 except Exception as e:
                     print("Exception while creating dictionary: ",e)
 
-                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
+                mydict = {"Price": price,"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
             return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
